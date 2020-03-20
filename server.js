e92b81bb2764dfe0a290780bddcda54cfd00c0ca
Eduard Fedoruk <ed.fedorukk@gmail.com>
2020-03-20 14:23:23 +0100

    init

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	console.log(req)
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/test-status', (req, res) => {
+	res.sendStatus(200);
+});
+
+app.get('/test-data', (req, res) => {
+	res.json({ test: true });
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
